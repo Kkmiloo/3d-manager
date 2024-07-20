5784a4daef323163d397e1306e7f6f75543a6259
kkmiloo <camilo.reyes_granada@uao.edu.co>
2024-07-19 23:58:16 -0500

    add:  material basics

---
+++ src/material/material.service.ts
@@ -0,0 +1,48 @@
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { CreateMaterialDto } from './dto/create-material.dto';
+import { UpdateMaterialDto } from './dto/update-material.dto';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class MaterialService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+  create(createMaterialDto: CreateMaterialDto) {
+    const { name, type } = createMaterialDto;
+
+    const material = this.material.create({
+      data: {
+        name,
+        type,
+      },
+    });
+
+    return material;
+  }
+
+  findAll() {
+    const materials = this.material.findMany();
+
+    return materials;
+  }
+
+  async findOne(id: string) {
+    const material = await this.material.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!material) throw new NotFoundException('Material not found');
+
+    return material;
+  }
+
+  update(id: number, updateMaterialDto: UpdateMaterialDto) {
+    return `This action updates a #${id} material`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} material`;
+  }
+}
