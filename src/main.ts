56f719f6b76e1cb4cd95592ab29afe7be055677b
kkmiloo <camilo.reyes_granada@uao.edu.co>
2024-07-19 23:58:59 -0500

    add: validation pipe - global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Remove properties that are not defined in the DTO
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are present
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
